e1127867faab7bda238f2f86bf7ad0d3463ef6b6
Lynn Eades <lynn.eades@gmail.com>
2015-05-01 20:05:12 +0000

    rspec setup, adding heroku configs, seeds for leads

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do |i|
+  Lead.create!(name: "Jordan Hudgens #{i}", company: "ABC Compnay", location: "Lubbock", phone: "1234567890")
+end
+
+50.times do |i|
+  Lead.create!(name: "Tiffany Ledford #{i}", company: "ABC Compnay", location: "Midland", phone: "1112223330")
+end
+
+50.times do |i|
+  Lead.create!(name: "Chase Baker #{i}", company: "XYZ Compnay", location: "Denver", phone: "8886661234")
+end
